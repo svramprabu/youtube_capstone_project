c13f0a2262a73f8501cf2667e500637c92a07a21
svramprabu <111267339+svramprabu@users.noreply.github.com>
2023-05-15 12:50:11 +0530

    Update Creating_Dataframes and Loading in SQL.py

--- pages/Creating_Dataframes and Loading in SQL.py
+++ pages/Creating_Dataframes and Loading in SQL.py
@@ -196,9 +196,9 @@ if __name__ == "__main__":
                 db_Info = mydb.get_server_info()
                 st.write("Connected to MySQL Server version ", db_Info)
                 cursor = mydb.cursor()
-                cursor.execute("CREATE DATABASE if not exists yt_details")
-                #cursor.execute("select database();")
-                cursor.execute("use yt_details")
+                #cursor.execute("CREATE DATABASE if not exists yt_details")
+                cursor.execute("select database();")
+                #cursor.execute("use yt_details")
                 record = cursor.fetchone()
                 st.write("You're connected to database: ", record)
 
