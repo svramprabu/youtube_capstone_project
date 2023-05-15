efd340e50720f54cdc83f8fc3a37e4e9c2bcafc9
svramprabu <111267339+svramprabu@users.noreply.github.com>
2023-05-15 10:46:49 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,36 @@
+import os
+import pymongo
 import streamlit as st
+from google.auth.transport.requests import Request
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+import pickle
+import numpy as np
+import pandas as pd
+import mysql.connector
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
-st.write("success")
+
+
+
+st.title('Youtube Data Scraping app:chart_with_upwards_trend:')
+
+scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
+
+@st.cache_data
+def youtube_authenticate():
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    #os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    #client_secrets_file = r"C:\Users\SVR\Python vs code\Guvi_Projects\credentials.json"
+    dev_key = "AIzaSyAJSNdqINYUD9nzb39D4MUPYrWw-s6rb9c"
+    return googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey = dev_key)
+  
+if __name__ == "__main__":
+  youtube = youtube_authenticate()
