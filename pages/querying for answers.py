8140b1ce1d3954fe0059d198bb03b79520372e75
svramprabu <111267339+svramprabu@users.noreply.github.com>
2023-05-15 16:49:50 +0530

    Update querying for answers.py

--- pages/querying for answers.py
+++ pages/querying for answers.py
@@ -16,9 +16,9 @@ if __name__ == "__main__":
             db_Info = mydb.get_server_info()
             st.write("Connected to MySQL Server version ", db_Info)
             cursor = mydb.cursor()
-            #cursor.execute("CREATE DATABASE if not exists yt_details")
+            
             cursor.execute("select database();")
-            #cursor.execute("use yt_details")
+            
             record = cursor.fetchone()
             st.write("You're connected to database: ", record)
             if st.button(" Q1 What are the names of all the videos and their corresponding channels?"):
@@ -28,21 +28,21 @@ if __name__ == "__main__":
                 st.write(ans1)
 
             if st.button("Q2 Which channels have the most number of videos, and how many videos do they have?"):
-                Q2="SELECT * FROM yt_details.channel_det ORDER BY Number_of_Videos DESC"
+                Q2="SELECT * FROM sql12618369.channel_det ORDER BY Number_of_Videos DESC"
                 ans2 = pd.read_sql(Q2,mydb)
                 st.write(f"Query: {Q2}")
                 st.write(ans2[['Channel_Name','Number_of_Videos']].loc[0])
 
             # 
             if st.button("Q3 What are the top 10 most viewed videos and their respective channels?"):
-                a = pd.read_sql("SELECT Channel_Name FROM yt_details.channel_det",mydb)
+                a = pd.read_sql("SELECT Channel_Name FROM sql12618369.channel_det",mydb)
                 channels_list = []
                 for i in range(len(a)):
                     channels_list.append(a.loc[i].values[0])
 
                 ans3= pd.DataFrame()
                 for each_channel in channels_list:
-                    Q3=f"SELECT * FROM yt_details.video_det WHERE channelTitle='{each_channel}' ORDER BY viewCount DESC LIMIT 10"
+                    Q3=f"SELECT * FROM sql12618369.video_det WHERE channelTitle='{each_channel}' ORDER BY viewCount DESC LIMIT 10"
                     ans3=pd.concat([ans3,pd.read_sql(Q3,mydb)],ignore_index=True)
                     st.write(f"Query: {Q3}")
                 st.write(ans3[['video_title','channelTitle','viewCount']])
@@ -65,14 +65,14 @@ if __name__ == "__main__":
 
             # 
             if st.button("Q6 What is the total number of likes and dislikes for each video, and what are their corresponding video names?"):
-                Q6="SELECT video_det.video_title,video_det.likeCount FROM yt_details.video_det ORDER BY video_det.likeCount DESC LIMIT 10"
+                Q6="SELECT video_det.video_title,video_det.likeCount FROM sql12618369.video_det ORDER BY video_det.likeCount DESC LIMIT 10"
                 ans6 = pd.read_sql(Q6,mydb)
                 st.write(f"Query {Q6}")
                 st.write(ans6)
 
             # 
             if st.button("Q7 What is the total number of views for each channel, and what are their corresponding channel names?"):
-                Q7 = "SELECT channel_det.Channel_Name,channel_det.Channel_Views FROM yt_details.channel_det"
+                Q7 = "SELECT channel_det.Channel_Name,channel_det.Channel_Views FROM sql12618369.channel_det"
                 ans7 = pd.read_sql(Q7,mydb)
                 st.write(f"Query: {Q7}")
                 st.write(ans7)
@@ -86,7 +86,7 @@ if __name__ == "__main__":
 
             # 
             if st.button("Q9 What is the average duration of all videos in each channel, and what are their corresponding channel names?"):
-                Q9 = "SELECT video_det.channelTitle, sum(video_det.duration)/count(video_det.video_id) AS average_duration_in_seconds FROM yt_details.video_det group by video_det.channelTitle"
+                Q9 = "SELECT video_det.channelTitle, sum(video_det.duration)/count(video_det.video_id) AS average_duration_in_seconds FROM sql12618369.video_det group by video_det.channelTitle"
                 ans9=pd.read_sql(Q9,mydb)
                 st.write(f"Query {Q9}")
                 st.write(ans9)
