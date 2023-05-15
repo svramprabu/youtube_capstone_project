36ced303f313f89dd4663cb00bb0487ed1586545
svramprabu <111267339+svramprabu@users.noreply.github.com>
2023-05-15 11:21:38 +0530

    Update test.py

--- test.py
+++ test.py
@@ -34,4 +34,15 @@ def youtube_authenticate():
   
 if __name__ == "__main__":
   youtube = youtube_authenticate()
-  st.write(youtube)
+  #st.write(youtube)
+  uri = "mongodb+srv://svrdb:svrnoobs@ytdatabysvr.0dp48ba.mongodb.net/?retryWrites=true&w=majority"
+
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
+
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        st.write("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        st.write(e)
