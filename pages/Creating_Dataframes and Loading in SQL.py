bdfef733f4ea97d94c6720af7dfee7e1f980cdf0
svramprabu <111267339+svramprabu@users.noreply.github.com>
2023-05-15 20:27:33 +0530

    Update Creating_Dataframes and Loading in SQL.py
    channels df was left undefined

--- pages/Creating_Dataframes and Loading in SQL.py
+++ pages/Creating_Dataframes and Loading in SQL.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     for each_channel_id in channel_db.find():
         col_channel = list(each_channel_id['Channel_Details'].keys())
         break
-    #channels_df = pd.DataFrame(list(channel_db.find(),columns = col_channel)        
+    channels_df = pd.DataFrame(list(channel_db.find(),columns = col_channel)        
     for each_channel_id in channel_db.find():
         channels_df=pd.concat([channels_df, pd.Series(each_channel_id['Channel_Details']).to_frame().T],ignore_index=True)
 
